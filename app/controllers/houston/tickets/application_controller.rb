16366b3506f65338f183e896edd3a48d9b861142
Bob Lail <bob.lailfamily@gmail.com>
2017-01-16 08:10:32 -0600

    [skip] Extracted rescue_from from houston-core (1m)

--- app/controllers/houston/tickets/application_controller.rb
+++ app/controllers/houston/tickets/application_controller.rb
@@ -1,5 +1,15 @@
 module Houston::Tickets
   class ApplicationController < ::ApplicationController
     layout "houston/tickets/application"
+
+    rescue_from Github::Unauthorized do |exception|
+      session["user.return_to"] = request.referer
+      if request.xhr?
+        head 401, "X-Credentials" => "Oauth", "Location" => main_app.oauth_consumer_path(id: "github")
+      else
+        redirect_to main_app.oauth_consumer_path(id: "github")
+      end
+    end
+
   end
 end
